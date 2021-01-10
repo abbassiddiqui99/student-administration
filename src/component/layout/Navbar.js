a099871d6c90f1f249e9eaf56e701457efd29332
abbassiddiqui99 <49212607+abbassiddiqui99@users.noreply.github.com>
2021-01-10 22:11:15 +0500

    first commit

---
+++ src/component/layout/Navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-sm navbar-light bg-white">
+      <div className="container">
+        <a className="navbar-brand" href="/">
+          <img
+            src="assets/images/administrator.png"
+            height="50px"
+            alt="admin"
+          />
+        </a>
+
+        <div>
+          <ul className="navbar-nav mr-auto"></ul>
+          <ul className="navbar-nav align-items-center">
+            <li className="nav-item nav-item-mobile">
+              <a href="#!" className="btn btn-primary mr-3">
+                Add Student
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-a dropdown-toggle"
+                href="!#"
+                id="navbarDropdown"
+                data-toggle="dropdown"
+              >
+                <img
+                  src="assets/images/administrator.png"
+                  height="30px"
+                  alt="admin_image"
+                />
+                <span className="ml-2 navbar-text">Subroto Biswas</span>
+              </a>
+              <div className="dropdown-menu">
+                <a className="dropdown-item bg-dark text-white" href="!#">
+                  Add Student
+                </a>
+                <a className="dropdown-item bg-dark text-white" href="!#">
+                  Profile
+                </a>
+                <a className="dropdown-item bg-dark text-white" href="!#">
+                  Logout
+                </a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="!#">
+                  Ads
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/component/studentDetailsCard/StudentCard.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Avatar from "react-avatar";
+
+const StudentCard = () => {
+  return (
+    <div className="container">
+      <div className="py-4">
+        <div className="row">
+          {/* <!-- RUN LOOP HERE -->*/}
+          <div className="col-lg-3 col-md-6 mb-4" key="PUT_HERE_KEY">
+            <div className="card shadow text-center py-4 shadow-lg mt-5">
+              {/* <!-- Profile Image  --> */}
+              <Avatar
+                name="Wim Mostmans"
+                size="80"
+                round={true}
+                color="#000"
+                style={{ margin: "auto" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title mb-0">Student Name</h5>
+                <p className="text-muted small">Student E-mail</p>
+                <a href="!#" className="btn btn-primary btn-profile">
+                  View Profile
+                </a>
+                <button className="btn btn-edit">
+                  <span className="material-icons">delete_outline</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StudentCard;
